e0cfc2bb4e2982777ea375854e030b7b22d18a60
toastoffee <931391442@qq.com>
2024-09-13 21:14:39 +0800

    add render_window.hpp

---
+++ src/render_window.hpp
@@ -0,0 +1,22 @@
+/**
+  ******************************************************************************
+  * @file           : render_window.hpp
+  * @author         : toastoffee
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2024/9/13
+  ******************************************************************************
+  */
+
+
+
+#ifndef REALTIME_RAYTRACER_RENDER_WINDOW_HPP
+#define REALTIME_RAYTRACER_RENDER_WINDOW_HPP
+
+
+class RenderWindow {
+
+};
+
+
+#endif //REALTIME_RAYTRACER_RENDER_WINDOW_HPP
