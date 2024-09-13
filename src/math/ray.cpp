b032804f51f99182b1f0dd7b2b55bd65fd8ef994
toastoffee <931391442@qq.com>
2024-09-13 14:53:08 +0800

    ray implementations

--- src/math/ray.cpp
+++ src/math/ray.cpp
@@ -12,3 +12,17 @@
 
 #include "ray.hpp"
 
+Ray::Ray(const Vec3 &origin, const Vec3 &dir)
+: m_origin(origin), m_dir(dir) { }
+
+Vec3 Ray::origin() const {
+    return m_origin;
+}
+
+Vec3 Ray::direction() const {
+    return m_dir;
+}
+
+Vec3 Ray::GetPoint(double t) const {
+    return m_origin + t * m_dir;
+}
