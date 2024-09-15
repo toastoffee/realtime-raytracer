8c43fdafe82a2b8073eb50c6202d13eb0fb1fb2e
toastoffee <931391442@qq.com>
2024-09-15 13:54:32 +0800

    add camera class

---
+++ src/camera.hpp
@@ -0,0 +1,22 @@
+/**
+  ******************************************************************************
+  * @file           : camera.hpp
+  * @author         : toastoffee
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2024/9/15
+  ******************************************************************************
+  */
+
+
+
+#ifndef REALTIME_RAYTRACER_CAMERA_HPP
+#define REALTIME_RAYTRACER_CAMERA_HPP
+
+
+class Camera {
+
+};
+
+
+#endif //REALTIME_RAYTRACER_CAMERA_HPP
