c9811acb21d5e2dedf13a34f80b1d2e034b2667b
toastoffee <931391442@qq.com>
2024-09-25 16:22:28 +0800

    modify sky box

--- main.cpp
+++ main.cpp
@@ -1,17 +1,20 @@
 #include "path_tracer.hpp"
 #include "sphere.hpp"
 
+#include "skybox.hpp"
 
 int main() {
 
-    std::unique_ptr<Scene> scene(new Scene());
+//    std::unique_ptr<Scene> scene(new Scene());
+//
+//    scene->AddObject(std::unique_ptr<Sphere>(new Sphere(Vec3(0, 0, 1), 0.5f)));
+//    scene->AddObject(std::unique_ptr<Sphere>(new Sphere(Vec3::down()*100.5, 100.f)));
+//
+//    std::unique_ptr<PathTracer> app(new PathTracer(scene.get()));
+//
+//    app->MainLoop();
 
-    scene->AddObject(std::unique_ptr<Sphere>(new Sphere(Vec3(0, 0, 1), 0.5f)));
-    scene->AddObject(std::unique_ptr<Sphere>(new Sphere(Vec3::down()*100.5, 100.f)));
-
-    std::unique_ptr<PathTracer> app(new PathTracer(scene.get()));
-
-    app->MainLoop();
+    SkyBox skyBox("../static/skybox/");
 
     return 0;
 }
